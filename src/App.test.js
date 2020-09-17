33bd6da6c5a12e3c4a1b6e6a02bb318be724622c
Ruby0626 <69333143+Ruby0626@users.noreply.github.com>
2020-09-17 13:59:28 +1200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,18 +3,6 @@ import { render } from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-test("Renders 'Register to SEER'", () => {
-  const { getByText } = render(<App />);
-  const HeroElement = getByText(/Register to SEER/i);
-  expect(HeroElement).toBeInTheDocument();
-});
-
-test("Renders 'Login to SEER'", () => {
-  const { getByText } = render(<App />);
-  const TeamElement = getByText(/Login to SEER/i);
-  expect(TeamElement).toBeInTheDocument();
-});
-
 test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
