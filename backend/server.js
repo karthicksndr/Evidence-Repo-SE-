0322d7d6a0515dff1401f28594f1570dc281ab60
Ruby0626 <69333143+Ruby0626@users.noreply.github.com>
2020-10-02 14:29:01 +1300

    Heroku updates

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,9 @@ app.use('/auth',authRouter)
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static( '/client/build' ));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+      });
 }
 
 app.listen(port, () => {
