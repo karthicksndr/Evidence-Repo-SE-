199548715e8b7dcf38dac37d0e64700611785f59
Ruby0626 <69333143+Ruby0626@users.noreply.github.com>
2020-10-16 13:47:50 +1300

    Eslint and TravisCI changes

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,50 +4,50 @@
 3. password
 4. email
 5. user type
-6. search history: 
+6. search history:
 7. createdAt
 */
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true, 
-        maxlength: 32,
-        trim: true
-    }, 
-    lastName: {
-        type: String, 
-        required: true, 
-        maxlength: 32,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true, 
-        minlength: 5,
-        trim: true
-    }, 
-    email : {
-        type: String,
-        required: true, 
-        unique: true, 
-        trim: true
-    },
-    userType : {
-        type: String,
-   //   required: true
-    },
-    searchHistory : {
-        type: Array,
-    }
- },
-    {
-        timestamps : true
-    }
+  firstName: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  userType: {
+    type: String,
+    //   required: true
+  },
+  searchHistory: {
+    type: Array,
+  },
+},
+{
+  timestamps: true,
+},
 );
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,14 @@
-const express = require("express")
+const express = require('express');
 const router = express.Router();
 
-const { getUserById, getUser, updateUser, getAllUsers } = require('../controllers/user')
-const {isSignedIn,isAdmin,isAuthenticated} = require('../controllers/auth')
+const {getUserById, getUser, updateUser, getAllUsers} = require('../controllers/user');
+const {isSignedIn, isAdmin, isAuthenticated} = require('../controllers/auth');
 
-router.param("userId" ,getUserById);
+router.param('userId', getUserById);
 
 // router.get("/:userId", isSignedIn, isAuthenticated, getUser);
 
-router.get("/users/all", getAllUsers);
+router.get('/users/all', getAllUsers);
 
 // router.put("/:userId", isSignedIn, isAdmin, isAuthenticated, updateUser)
 
